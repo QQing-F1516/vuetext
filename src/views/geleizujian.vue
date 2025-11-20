e2c0ffe342f8eb2d69d1255b31b22260294dc9e9
syxyQQ <2060781487@qq.com>
2025-11-20 22:09:44 +0800

    11.20-2

--- src/views/geleizujian.vue
+++ src/views/geleizujian.vue
@@ -1,65 +1,61 @@
 <template>
-  <div class="divdiv" v-if="showContent">
-    <h3 class="title">分类组件+++++</h3>
-    <nav class="navigation">
-      <router-link to="/new" class="nav-link">跳转到new</router-link>
-    </nav>
-    <hr>
-    <swiper></swiper>
-    <hr>
-  </div>
-</template>
-
-<script>
-
-import swiper from "../nnewcomponents/swiper.vue"
-
-export default {
-  name: 'geleizujian',
-  data() {
-    return {
-      showContent: false,
+    <div class="divdiv" v-if="showContent">
+      <h3 class="title">各类组件+++++</h3>
+      <nav class="navigation">
+        <router-link to="/new" class="nav-link">跳转到new</router-link>
+      </nav>
+      <hr>
+      <Swiper></Swiper>
+      <elementbiadan></elementbiadan>
+    </div>
+  </template>
+  
+  <script>
+  import Swiper from '../nnewcomponents/swiper.vue'
+  import elementbiadan from '../nnewcomponents/elementbiaodan.vue';
+  
+  export default {
+    name: 'geleizujian',
+    components: {
+      Swiper,
+      elementbiadan
+    },
+    data() {
+      return {
+        showContent: false,
+      }
+    },
+    mounted() {
+      setTimeout(() => {
+        this.showContent = true;
+      }, 0);
     }
-  },
-  components: {
-    swiper
-  },
-
-  mounted() {
-    setTimeout(() => {
-      this.showContent = true;
-    }, 0);
   }
-}
-</script>
-
-<style scoped>
-.divdiv {
-  position: relative;
-  z-index: 11;
-}
-
-.title {
-  color: red;
-  margin-bottom: 20px;
-}
-
-.navigation {
-  margin: 20px 0;
-  z-index: 11;
-}
-
-.nav-link {
-  display: inline-block;
-  padding: 10px 20px;
-  background-color: rgba(255, 255, 255, 0.1);
-  color: white;
-  text-decoration: none;
-  border-radius: 5px;
-  transition: background-color 0.3s ease;
-}
-
-.nav-link:hover {
-  background-color: rgba(255, 255, 255, 0.3);
-}
-</style>
+  </script>
+  
+  <style scoped>
+  .divdiv {
+    position: relative;
+    z-index: 10;
+  }
+  .title {
+    color: red;
+    margin-bottom: 20px;
+  }
+  .navigation {
+    margin: 20px 0;
+    z-index: 11;
+  }
+  .nav-link {
+    display: inline-block;
+    padding: 10px 20px;
+    background-color: rgba(255, 255, 255, 0.1);
+    color: white;
+    text-decoration: none;
+    border-radius: 5px;
+    transition: background-color 0.3s ease;
+  }
+  .nav-link:hover {
+    background-color: rgba(255, 255, 255, 0.3);
+  }
+  </style>
